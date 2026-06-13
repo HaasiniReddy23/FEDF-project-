4a0ca478899181a8287b7c8fc1e79115b06f0e2c
Haasini Reddy <haasini.tt@gmail.com>
2026-06-13 13:26:02 +0530

    Create App.jsx

---
+++ version 1/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
